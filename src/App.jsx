670466252b7695f1f4b2881a1c26912e7792b101
bdaneels <brecht.daneels@hotmail.com>
2023-08-17 09:50:13 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Header from "./components/header";
+
+function App() {
+  return (
+    <div className="flex flex-col bg-slate-500 h-screen w-screen justify-start">
+      <Header />
+      {
+        //header
+        //div left to right
+        //form div
+        //cv div
+      }
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import "../App.css";
+function Header() {
+  return (
+    <>
+      <div className="text-xl rounded-md bg-white w-full py-12 px-8 mx-auto text-center ">
+        {" CV Builder"}
+      </div>
+    </>
+  );
+}
+
+export default Header;
